f22ab2c37ff812384139a42517dc6a84dfa0cec7
RoseTori <cojdanil00@gmail.com>
2025-10-17 15:49:34 +0900

    Created dog and timer beans that are connected to AnimalCage

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,9 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("timer")
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
